0adc57d2ac5f7c70df238ab1e09028cefae144ff
dawiddd95 <dawlyc@wp.pl>
2020-05-30 19:07:29 +0200

    Reply model created

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  resources :discussions
+  resources :discussions do
+    resources :replies
+  end
+
   root 'discussions#index'
 
   devise_for :users, controllers: {registrations: 'registrations'}
